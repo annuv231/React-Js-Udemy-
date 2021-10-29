065fb147f752c50f38d0c11512d9a39c2a235e76
annuv231 <annuv231@gmail.com>
2021-10-29 22:26:31 +0530

    added arrowfunction.js file

---
+++ Javacript/arrowFunction.js
@@ -0,0 +1,11 @@
+//normal function
+function printMyName(name){
+    console.log(name+" Verma");
+}
+printMyName("Amlendra");
+//Arrow function
+//arrow function is used for writing shorter code..
+const printmyName=name=>{
+    console.log(name+" verma");
+}
+printmyName("Ankush");
